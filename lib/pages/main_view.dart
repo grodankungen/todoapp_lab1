9af8e9d38efd926bf75446198df4d8f788d4fd6a
Kalle <kalle@kakmonstret.se>
2026-03-25 14:49:14 +0100

    mer viktigt!

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/widgets/task_list.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -10,15 +11,7 @@ class MainView extends StatelessWidget {
              title: const Text('Things todo'),
              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('DAT216/TIG091'),
-            Text('Lab1 Todos'),
-          ],
-        ),
-      ),
-    );
+      body: TaskList()
+  );
   }
 }
