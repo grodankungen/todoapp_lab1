b3a23925ee8e1c8512f152fe0b7d0d30a1bb2159
Kalle <kalle@kakmonstret.se>
2026-03-25 15:33:23 +0100

    checkpoint

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/pages/add_view.dart';
 import 'package:lab1/widgets/task_list.dart';
 
 class MainView extends StatelessWidget {
@@ -7,11 +8,22 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-             title: const Text('Things todo'),
-             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          ),
-      body: TaskList()
-  );
+        appBar: AppBar(
+          title: const Text('Things todo'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: TaskList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddView(),
+              ),
+            );
+          },
+          tooltip: 'Add todo',
+          child: const Icon(Icons.add),
+        ));
   }
 }
